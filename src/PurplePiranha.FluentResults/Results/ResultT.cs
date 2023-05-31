2cb2768bc923f849a8fdfaa5ec577fa05d59e0e0
Paul - Purple Piranha <paul@purplepiranha.co.uk>
2023-05-31 11:21:17 +0100

    Seperate Result and ResultT to seperate files

---
+++ src/PurplePiranha.FluentResults/Results/ResultT.cs
@@ -0,0 +1,26 @@
+﻿using PurplePiranha.FluentResults.Errors;
+
+namespace PurplePiranha.FluentResults.Results;
+
+public class Result<TValue> : Result
+{
+    #region Fields
+    private readonly TValue? _value;
+    #endregion
+
+    #region Ctr
+    protected internal Result(TValue? value) : base() => _value = value;
+    protected internal Result(TValue? value, Error error) : base(error) => _value = value;
+    protected internal Result(TValue? value, IEnumerable<string> validationErrors) : base(validationErrors) => _value = value;
+    #endregion
+
+    #region Public properties
+    public TValue Value => IsSuccess
+        ? _value!
+        : throw new InvalidOperationException("The value of a failure result can not be accessed.");
+    #endregion
+
+    #region Operators
+    public static implicit operator Result<TValue>(TValue? value) => Create(value);
+    #endregion
+}
